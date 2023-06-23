a106a1fee44fccbd43a81db951d2f3f1d1e7f19e
oveey <ovieigere@gmail.com>
2023-06-23 01:17:32 +0100

    added to my portfolio

--- demo/demo.js
+++ demo/demo.js
@@ -12,3 +12,35 @@
 		}
 	});
 });
+
+// Wait for the DOM to load
+    document.addEventListener("DOMContentLoaded", function() {
+        // Get all the portfolio navigation items
+        const portfolioNavItems = document.querySelectorAll("#filter-button li");
+        // Get all the grid items
+        const gridItems = document.querySelectorAll(".grid-item");
+
+        // Add click event listeners to portfolio navigation items
+        portfolioNavItems.forEach(function(navItem) {
+            navItem.addEventListener("click", function() {
+                // Remove the "current" class from all portfolio navigation items
+                portfolioNavItems.forEach(function(item) {
+                    item.classList.remove("current");
+                });
+                // Add the "current" class to the clicked portfolio navigation item
+                this.classList.add("current");
+
+                // Get the filter value from the clicked portfolio navigation item
+                const filterValue = this.getAttribute("data-filter");
+
+                // Show/hide the grid items based on the filter value
+                gridItems.forEach(function(item) {
+                    if (item.classList.contains(filterValue)) {
+                        item.classList.remove("hidden");
+                    } else {
+                        item.classList.add("hidden");
+                    }
+                });
+            });
+        });
+    });
